59b9836d831e8a21c6e596e6a8f516d15869683a
ser-rum <101265858+ser-rum@users.noreply.github.com>
2022-04-15 16:29:06 +0300

    Delete Where`sMyTV.py

--- Where`sMyTV.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-try:
-    browser = webdriver.Chrome()
-    browser.get('https://www.dns-shop.ru')
-    search = browser.find_element_by_class_name('ui-input-search__input ui-input-search__input_presearch')
-
-finally:
-    browser.quit()
