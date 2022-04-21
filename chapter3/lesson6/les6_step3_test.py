3f72a05792fb0affbca98de6ba656ffc50be4d3f
ser-rum <serj1988rum@gmail.com>
2022-04-21 11:20:04 +0300

    Adding my best work for the recent time!

--- chapter3/lesson6/les6_step3_test.py
+++ chapter3/lesson6/les6_step3_test.py
@@ -3,24 +3,24 @@ from selenium import webdriver
 import time
 import math
 
-answer = math.log(int(time.time()))
+def answer():
+    answer = math.log(int(time.time()))
+    return answer
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
+    browser.implicitly_wait(15)
     yield browser
     print("\nquit browser..")
+    browser.quit()
 
-@pytest.mark.parametrize('lesson', [236895])
+@pytest.mark.parametrize('lesson', [236895, 236896, 236897, 236898, 236899, 236903, 236904, 236905])
 def test_guest_should_see_login_link(browser, lesson):
     link = f"https://stepik.org/lesson/{lesson}/step/1"
     browser.get(link)
-    textarea = browser.find_element_by_class("textarea")
-    textarea.send_keys(answer)
-    browser.find_element_by_class("button").click()
-    
-
-
-'''', 236896, 236897, 236898, 236899, 236903, 236904, 236905'''
+    browser.find_element_by_class_name('textarea').send_keys(answer())
+    browser.find_element_by_class_name("submit-submission").click()
+    text = browser.find_element_by_class_name("smart-hints__hint").text
+    assert text == "Correct!", text
